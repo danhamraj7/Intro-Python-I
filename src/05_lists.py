dfacae0f9780f50a50ac11f9a12aa744a44fad62
danhamraj7 <ddan_7@hotmail.com>
2020-07-06 18:01:42 -0400

    mvp on files 1-10

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,29 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
-print(x)
+x.append(4)
+print("appended 4 to 'x'", x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
-# YOUR CODE HERE
-print(x)
+x.extend(y)
+print("extend x into y", x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
 print(x)
+x.remove(8)
+print("removed '8' from 'x'", x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
-print(x)
+x.insert(5, 99)
+print("inserted '99' at fifth position in 'x'", x)
 
 # Print the length of list x
 # YOUR CODE HERE
+print(" no. of items in 'x'", len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+for item in x:
+    print("each item in 'x' multiply by 1000", item * 1000)
